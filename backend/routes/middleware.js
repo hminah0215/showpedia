01c9495fbc4b4c5f101c1b6ae04968f3f5daebb7
hminah0215 <hyeonminah@gmail.com>
2021-07-26 12:17:15 +0900

    [feat] 로그인,로그아웃 상태 모두 접근가능한 미들웨어

--- backend/routes/middleware.js
+++ backend/routes/middleware.js
@@ -4,9 +4,6 @@
 exports.isLoggedIn = (req, res, next) => {
   // 쿠키에 사용자 인증 토큰을 저장하니까, 쿠키에 member 라는 이름의 값이 있는지를 확인
   // 쿠키에 있는 member값을 req객체의 user에 담고 있으면 login상태, 없으면 로그인필요
-  req.user = req.cookies.member ? 'login' : '';
-  console.log('req.cookies.member', req.cookies.member);
-  console.log('req.user가 있는지?', req.user);
 
   // req객체에 isAuthenticated 메서드를 추가, 로그인 중이면 true!
   req.user = req.cookies.member ? 'login' : '';
@@ -17,6 +14,7 @@ exports.isLoggedIn = (req, res, next) => {
   console.log('isLoggedIn에서의 isAuth', req.isAuthenticated());
 
   if (req.isAuthenticated()) {
+    console.log('isLoggedIn에서의 isAuth 로그인 성공 if문', req.isAuthenticated());
     next();
   } else {
     res.status(403).send('로그인 필요한 상태입니다.');
@@ -26,9 +24,22 @@ exports.isLoggedIn = (req, res, next) => {
 // 로그인 안한 상태인지
 exports.isNotLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    console.log('로그인 안한 상태 콘솔');
+    console.log('isNotLoggedIn에서 isAuthenticated', req.isAuthenticated());
     next();
   } else {
+    console.log('isNotLoggedIn에서 else 로그인한 상태', req.isAuthenticated());
     const message = encodeURIComponent('로그인한 상태입니다.');
     res.redirect(`/?error=${message}`);
+  }
+};
+
+/*** 둘다 접근 가능 미들웨어 ***/
+exports.allLogged = (req, res, next) => {
+  // 로그인 안해도 접근가능
+  if (req.isAuthenticated() || !req.isAuthenticated()) {
+    next();
+  } else {
+    console.log('둘다 접근 가능');
   }
 };
