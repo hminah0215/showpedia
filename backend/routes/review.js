26cb5674ba8c57999c46f783bf2f89b383fec5ff
ayo <12ayo22@gmail.com>
2021-07-27 17:05:48 +0900

    [feat] 리뷰 게시글 제공 api - 더미데이터

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -4,21 +4,45 @@ const router = express.Router();
 // DB
 const Review = require('../models').Review;
 
+// 해당 공연 id에 맞는 리뷰리스트 전부 가져오기
 router.get('/', (req, res) => {
-  console.log('리뷰에요~');
-  res.send('리뷰에요');
+  const reviewList = [
+    {
+      reviewNo: '1',
+      reviewContents: '더미 데이터',
+      reviewLikes: 33,
+      reviewReports: 10,
+      reviewStars: 4,
+      memberId: 'test',
+      // test용 더미 데이터 프로필 이미지
+      profilePhoto: 'https://www.w3schools.com/w3images/avatar6.png'
+    },
+    {
+      reviewNo: '1',
+      reviewContents: '더미 데이터',
+      reviewLikes: 33,
+      reviewReports: 10,
+      reviewStars: 4,
+      memberId: 'test',
+      profilePhoto: 'https://www.w3schools.com/w3images/avatar6.png'
+    }
+  ];
+  res.json({
+    code: '200',
+    data: reviewList,
+    msg: '리뷰 등록 성공'
+  });
 });
 
 // 리뷰 생성 라우터
 router.post('/', async (req, res) => {
   // 클라이언트에서 리뷰 정보를 가져온다.
-
   console.log('바디로 넘어오는 모든 것들', req.body);
   const review = {
     reviewStars: req.body.reviewStars,
     reviewContents: req.body.reviewContents,
     showId: req.body.showId, // 겨울왕국 [예산]
-    memberId: 'test'
+    memberId: 'test' // 로그인 미완성으로 임의 데이터
   };
 
   // 데이터를 저장한다.
